5ccce9140d398bb2fe9663377bb6bed26a26aa90
Rohan Chauhan <chauhanrohan607@gmail.com>
2026-06-23 23:54:23 +0530

    Setup & First Program

---
+++ FirstProgram.py
@@ -0,0 +1,42 @@
+print("hello world")
+print ("hello my name is rohan.","my age is 22")
+print(23)
+print(25)
+print(2+3)
+name = "Rohan"
+age = 23
+price = 25.99
+print(name)
+print(age)
+print(price)
+print("my name is",name)
+print(age)
+print(type(name))
+print(type(age))
+print(type(price))
+old = False
+a = None
+print(type(old))
+print(type(a))
+
+a = 2
+b = 3
+sum = a + b
+print(sum)
+#hekllojkcndkfndks
+""""hghghghgh"""
+
+print ((a == b) or ( b > a))
+print ((a == b) and ( b > a))
+
+c = 2
+d = 4.25 
+
+summ = c + d
+print(summ)
+
+e = int("2")
+f = 4.55
+print(type(e))
+summm = e + f
+print(summm)
